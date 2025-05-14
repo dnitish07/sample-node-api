5359f1e0b1803ece33014b064b16d5fed578e10a
Nitish Doddi <dnitishvvit2022@gmail.com>
2025-05-14 16:55:54 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from Node.js API!' });
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
